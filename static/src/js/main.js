fa84df92667f52ef2ead90017123e411c7c3dcdd
MrBin99 <paul.pages12@gmail.com>
2021-06-18 21:02:15 +0200

    feat(vite): Setup ViteJS

---
+++ static/src/js/main.js
@@ -0,0 +1,3 @@
+import '../css/style.css';
+
+alert('Hello from Django Vite example !');
---
+++ vite.config.js
@@ -0,0 +1,34 @@
+const { resolve } = require('path');
+
+module.exports = {
+  plugins: [],
+  root: resolve('./static/src'),
+  base: '/static/',
+  server: {
+    host: 'localhost',
+    port: 3000,
+    open: false,
+    watch: {
+      usePolling: true,
+      disableGlobbing: false,
+    },
+  },
+  resolve: {
+    extensions: ['.js', '.json'],
+  },
+  build: {
+    outDir: resolve('./static/dist'),
+    assetsDir: '',
+    manifest: true,
+    emptyOutDir: true,
+    target: 'es2015',
+    rollupOptions: {
+      input: {
+        main: resolve('./static/src/js/main.js'),
+      },
+      output: {
+        chunkFileNames: undefined,
+      },
+    },
+  },
+};
